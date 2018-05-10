85c654965de83d4a9c0c77b5a99212023494f8b0
dedchord <15-10123@usb.ve>
2018-05-09 20:35:15 -0400

    Randomized_Quicksort funcional

--- LAB3/Randomized-Quicksort.py
+++ LAB3/Randomized-Quicksort.py
@@ -1,13 +1,28 @@
 #Randomized-Quicksort
 import random
 
-def Randomized-Quicksort(A=list, p=int, r=int):
-	if p < r
-		q = Randomized-Partition(A, p, r)
-		Randomized-Quicksort(A, p, q-1)
-		Randomized-Quicksort(A, q+1, r)
+def Partition(A=list, p=int, r=int):
+	Pivote = A[r]
+	i = p - 1
+	for j in range(p,r):
+		if A[j] <= Pivote:
+			i = i + 1
+			A[i],A[j]=A[j],A[i]
+	A[i+1],A[r]=A[r],A[i+1]
+	return i + 1
 
-def Randomized-Partition(array A, int p, int r):
-	i = Random(p, r)
-	Intercambiar A[i] con A[r]
+def Randomized_Partition(A=list, p=int, r=int):
+	i = random.randint(p, r)
+	A[i],A[r]=A[r],A[i]	
 	return Partition(A, p, r)
+
+def Randomized_Quicksort(A=list, p=int, r=int):
+	if p < r :
+		q = Randomized_Partition(A, p, r)
+		Randomized_Quicksort(A, p, q-1)
+		Randomized_Quicksort(A, q+1, r)
+	return A
+
+#Test:
+A=[1,3,4,5,5,3,5,8,99]
+print(Randomized_Quicksort(A,0,len(A)-1))
